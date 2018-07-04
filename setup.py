4c376389d4822920df89cfaa8e7a0db94745a391
makhilbabu <manamakhilbabu@gmail.com>
2018-07-03 21:32:20 -0500

    initial coding of setup file complete

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name = 'aggiestack',
+    version = '0.1'
     author = 'Akhil Babu Manam',
     install_requires = ['docopt'],
     entry_points = {
         'console_scripts': [
-            'aggiestack=aggiestack.cli:main',
+            'aggiestack=aggiestack.aggiestack:main',
         ],
     },
